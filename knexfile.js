d88edfa0d0b7b03b538c9e1719d5d2a18af37e33
dashlw <daisha.walton@du.edu>
2016-12-16 11:00:42 -0700

    :rocket: create heroku db

--- knexfile.js
+++ knexfile.js
@@ -1,9 +1,18 @@
 // Update with your config settings.
 
+require("dotenv").config();
+
 module.exports = {
 
-  development: {
-    client: 'pg',
-    connection: 'postgres://localhost/car_crud'
-  },
+    development: {
+        client: 'pg',
+        connection: 'postgres://localhost/car_crud'
+    },
+
+    production: {
+        client: 'pg',
+        connection: process.env.DATABASE_URL + "?ssl=true"
+    },
+
+
 };
